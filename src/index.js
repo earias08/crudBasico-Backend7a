fa702ee1bb763cc2fc9e132eec1a9143100a4507
Emilse <emilsearias@gmail.com>
2021-05-06 17:25:18 -0300

    se agregaron las configuracion iniciales

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
 
+// configuraciones
 // crear una instancia de express
 const app = express();
 
 // le pedimos a express que nos cree un port
-app.set('port', 4001);
+app.set('port', process.env.PORT || 4001);
 
 app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto '+app.get('port'));
+});
+
+// herramientas extras - middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'../public')));
+
+// creo una ruta
+app.get('/', (req, res)=>{
+    res.send('Hola esta es una prueba del backend');
 })
