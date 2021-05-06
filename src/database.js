9e63e0c4b787b326a09437670fd804f935732133
Emilse <emilsearias@gmail.com>
2021-05-06 17:51:54 -0300

    se agrego la conexion a la base de datos

---
+++ src/database.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const url= 'mongodb://localhost:27017/cafeteria7a';
+
+mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+
+// una vez establecida la conexion mostrar en la terminal
+connection.once('open',()=>{
+    console.log('BD conectada');
+});
